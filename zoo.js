3543c89ddc8ea5e080c68a653037068a517a98d4
David Domalik <david.domalik@gmail.com>
2021-03-15 15:00:10 -0400

    Initial commit with js functions created

---
+++ zoo.js
@@ -0,0 +1,20 @@
+function fish(name, age) {
+    this.name = name;
+    this.age = age;
+    this.image_alt = "Fish Icon";
+    this.image = "fish.png";
+}
+
+function lion(name, age) {
+    this.name = name;
+    this.age = age;
+    this.image_alt = "Lion Icon";
+    this.image = "lion.png";
+}
+
+function parrot(name, age) {
+    this.name = name;
+    this.age = age;
+    this.image_alt = "Parrot Icon";
+    this.image = "parrot.png";
+}
